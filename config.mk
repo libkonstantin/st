f486a2053370f5e0ff6d01b81334d2ea94c78bc0
Konstantin Kostin <kkostin@azul.com>
2022-06-26 01:45:37 +0700

    Use custom libXft

--- config.mk
+++ config.mk
@@ -7,16 +7,17 @@ VERSION = 0.8.5
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# Use bgra patched libXft
+X11LIB = -Wl,--start-group -lfontconfig -lfreetype -lXrender -lX11 -L../libxft-bgra/src/.libs -l:libXft.a -Wl,--end-group
+X11INC = -I/usr/include/freetype2 -I$(PWD)/../libxft-bgra/include
 
 PKG_CONFIG = pkg-config
 
 # includes and libs
-INCS = -I$(X11INC) \
+INCS = $(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+LIBS = $(X11LIB) -lm -lrt -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs fontconfig` \
        `$(PKG_CONFIG) --libs freetype2`
 
